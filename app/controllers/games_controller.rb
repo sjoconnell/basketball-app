63b926a45356cdb7190e0a0a8dfc847d7c57925d
Sam O'Connell <sjo773@gmail.com>
2015-07-12 17:01:05 -0500

    added restful controller actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,31 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def show
+    @game = Game.find_by(id: params[:id])
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.create(title: params[:title], address: params[:address], start_time: params[:start_time], end_time: params[:end_time], description: params[:description])
+    redirect_to "/games/#{@game.id}"
+  end
+
+  def edit
+    @game = Game.find_by(id: params[:id])
+  end
+
+  def update
+    @game = Game.find_by(id: params[:id])
+    @game.update(title: params[:title], address: params[:address], start_time: params[:start_time], end_time: params[:end_time], description: params[:description] )
+    redirect_to "/games/#{@game.id}"
+  end
+
+  def destroy
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'games#index'
 
+  get "/games" => 'games#index'
+  get "/games/new" => 'games#new'
+  get "/games/:id" => 'games#show'
+  post "/games" => 'games#create'
+  get "/games/:id/edit" => 'games#edit'
+  patch "/games/:id" => 'games#update'
+  delete "/games/:id" => 'geams#destroy'
+  post "/search" => 'games#search' 
+
 end
