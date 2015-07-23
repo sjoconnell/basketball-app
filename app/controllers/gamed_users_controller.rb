be4e09092c9149ebb52e807210c121bf1505b228
Sam O'Connell <sjo773@gmail.com>
2015-07-22 20:24:46 -0500

    players_joined updates correctly when user joins or leaves game

--- app/controllers/gamed_users_controller.rb
+++ app/controllers/gamed_users_controller.rb
@@ -13,6 +13,8 @@ class GamedUsersController < ApplicationController
 
   def create
     GamedUser.create(user_id: current_user.id, game_id: params[:game_id])
+    game = Game.find_by(id: params[:game_id])
+    game.update(players_joined: game.players_joined + 1)
     redirect_to "/games/#{params[:game_id]}"
   end
 
@@ -26,7 +28,9 @@ class GamedUsersController < ApplicationController
 
   def destroy
     @gamed_user = GamedUser.find_by(:id => params[:id])
+    game = Game.find_by(id: @gamed_user.game_id)
     @gamed_user.destroy
+    game.update(players_joined: game.players_joined - 1)
     redirect_to "/"
   end
 end
