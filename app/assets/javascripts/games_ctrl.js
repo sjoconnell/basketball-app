4b56de33fbae894bfca60d84170d565fef5f00d4
Sam O'Connell <sjo773@gmail.com>
2015-07-29 20:03:56 -0500

    adding one google map with markers of games to games index page

--- app/assets/javascripts/games_ctrl.js
+++ app/assets/javascripts/games_ctrl.js
@@ -12,15 +12,21 @@
     window.scope = $scope;
 
     function initialize() {
-        var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById('map'),
-            mapOptions);
+      var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+      var mapOptions = {
+        zoom: 4,
+        center: myLatlng
       }
-      
-      google.maps.event.addDomListener(window, 'load', initialize);
+      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+      var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Hello World!'
+      });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
 
   });
 
