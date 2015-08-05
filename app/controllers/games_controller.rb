dd38706cb6c7576ba779c67a75dcad4663ccceeb
Sam O'Connell <sjo773@gmail.com>
2015-08-04 19:21:22 -0500

    fixed game edit page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,6 +20,7 @@ class GamesController < ApplicationController
     @game = Game.new(title: params[:title], address: params[:address], start_time: params[:game][:start_time], end_time: params[:game][:end_time], players_joined: 1, players_allowed: params[:players_allowed], latitude: coordinates[0], longitude: coordinates[1], description: params[:description], status: "open", user_id: current_user.id)
     if @game.save
       GamedUser.create(user_id: current_user.id, game_id: @game.id)
+      flash[:success] = "Game Created"
       redirect_to "/games/#{@game.id}"
     else
       render :new
@@ -33,7 +34,8 @@ class GamesController < ApplicationController
   def update
     coordinates = Geocoder.coordinates(params[:address])
     @game = Game.find_by(id: params[:id])
-    if @game.update(title: params[:title], address: params[:address], start_time: params[:start_time], end_time: params[:end_time], description: params[:description], latitude: coordinates[0], longitude: coordinates[1] )
+    if @game.update(title: params[:title], address: params[:address], start_time: params[:game][:start_time], end_time: params[:game][:end_time], players_allowed: params[:players_allowed], description: params[:description], latitude: coordinates[0], longitude: coordinates[1] )
+      flash[:warning] = "Game Updated"
     redirect_to "/games/#{@game.id}"
     else
       render :edit
@@ -47,6 +49,7 @@ class GamesController < ApplicationController
       user.destroy
     end
     @game.destroy
+    flash[:danger] = "Game Deleted"
     redirect_to "/"
   end
 end
